ca1680df3efd0a1487dc774a4347255940105adc
test1 <test1@sina.com>
2014-12-19 09:55:35 +0800

    1

---
+++ dao_pro/src/main/java/com/test/dao/TestDaoImpl.java
@@ -0,0 +1,14 @@
+package com.test.dao;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+@Repository(value="testDao") 
+public class TestDaoImpl {
+	public ArrayList<String> load(){
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("a");
+		return al;  
+	}
+}
