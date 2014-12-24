0c02502775aed2b188132cafa5be14fe8e824f39
test1 <test1@sina.com>
2014-12-24 16:13:37 +0800

    entityManager

--- dao_pro/src/main/java/com/beasp/dao/TestDaoImpl.java
+++ dao_pro/src/main/java/com/beasp/dao/TestDaoImpl.java
@@ -2,14 +2,25 @@ package com.beasp.dao;
 
 import java.util.ArrayList;
 
-import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository; 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.beasp.bean.BeeTest;
 
 @Repository(value="testDao") 
-public class TestDaoImpl {
-	
-	public ArrayList<String> load(){ 
-		ArrayList<String> al = new ArrayList<String>();
-		al.add("a");
+@Transactional(propagation = Propagation.REQUIRED)
+public class TestDaoImpl{
+	@PersistenceContext
+	private EntityManager entityManager; 
+	public EntityManager getEntityManager() {
+	return entityManager;
+	};
+	public ArrayList<BeeTest> load(){ 
+		ArrayList<BeeTest> al= (ArrayList<BeeTest>)entityManager.createQuery(" FROM "+BeeTest.class.getSimpleName()+" AS BEE ").getResultList();
 		return al;  
 	}
 	
