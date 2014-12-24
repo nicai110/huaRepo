9d7cdb57697ea7531bf1a9c6a4a85d0d878b32bf
test1 <test1@sina.com>
2014-12-24 16:15:45 +0800

    -

--- service_pro/src/main/java/com/beasp/service/TestServiceImpl.java
+++ service_pro/src/main/java/com/beasp/service/TestServiceImpl.java
@@ -3,8 +3,9 @@ package com.beasp.service;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service; 
+import org.springframework.stereotype.Service;
 
+import com.beasp.bean.BeeTest;
 import com.beasp.dao.TestDaoImpl;
 
 @Service(value="testService")
@@ -12,7 +13,7 @@ public class TestServiceImpl  {
 	@Autowired
 	private TestDaoImpl testDao;
 	
-	public ArrayList<String> load(){
+	public ArrayList<BeeTest> load(){
 		return testDao.load();
 	}
 }
